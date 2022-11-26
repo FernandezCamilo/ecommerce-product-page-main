dc13b9f90293e309ce442f04c33d900722a5caa1
Camilo Fernández <fernandezcamilo022@gmail.com>
2022-11-26 03:12:11 -0300

    Mostrar el modal con el detalle del carrito

---
+++ main.js
@@ -0,0 +1,42 @@
+// Cambio de cantidad de articulos ingresado por el usuario
+
+let minusBtn = document.querySelector('.input__minus')
+let plusBtn = document.querySelector('.input__plus')
+let userInput = document.querySelector('.input__number')
+
+let userInputNumber = 0
+
+plusBtn.addEventListener('click', () => {
+  userInputNumber++
+  userInput.value = userInputNumber
+  console.log(userInputNumber)
+})
+
+minusBtn.addEventListener('click', () => {
+  userInputNumber--
+  if (userInputNumber <= 0) {
+    userInputNumber = 0
+  }
+  userInput.value = userInputNumber
+  console.log(userInputNumber)
+})
+
+// Agregar el total de productos al carrito cuando se presiona el boton ADD TO CART
+const addToCartBtn = document.querySelector('.details__button')
+let cartNotification = document.querySelector('.header__cart--notification')
+
+addToCartBtn.addEventListener('click', () => {
+  let lastValue = parseInt(cartNotification.innerText)
+  lastValue = lastValue + userInputNumber
+
+  cartNotification.innerText = lastValue
+  cartNotification.style.display = 'block'
+})
+
+// Mostrar el modal con el detaller del carrito
+const cartIconBtn = document.querySelector('.header__cart')
+const cartModal = document.querySelector('.cart-modal')
+
+cartIconBtn.addEventListener('click', () => {
+  cartModal.style.display = 'block'
+})
